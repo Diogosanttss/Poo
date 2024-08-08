16fc8ed08c2e471292b4cac8094197fd466e55c4
Diogo Conceição dos Santos <20231170150047@ifrn.local>
2024-08-08 17:11:41 -0300

    adicionando 'Quest1.cs'

---
+++ Quest1.cs
@@ -0,0 +1,15 @@
+using System;
+namespace Poo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = (1+4);
+            float a = 2.5f;
+            string e = "Olá";
+            bool s = false;
+            Console.WriteLine("{0}\n{1}\n{2}\n{3}",i,a.ToString("F2"),e, s);
+        }
+    }
+}
