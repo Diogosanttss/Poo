4759ad2aeaa5a335e3f1e50f8ff20270f964719e
Diogosanttss <diogo.santos@escolar.ifrn.edu.br>
2024-08-16 15:13:33 -0300

    adicionando 'Quest8.cs'

---
+++ Quest8.cs
@@ -0,0 +1,74 @@
+using System;
+
+
+class Program
+
+{
+
+    static void Main()
+
+    {
+
+        Console.Write("Digite o primeiro número: ");
+
+        int num1 = int.Parse(Console.ReadLine());
+
+
+        Console.Write("Digite o segundo número: ");
+
+        int num2 = int.Parse(Console.ReadLine());
+
+
+        Console.Write("Digite o terceiro número: ");
+
+        int num3 = int.Parse(Console.ReadLine());
+
+
+        int maior = num1;
+
+
+        if (num2 > maior)
+
+        {
+
+            maior = num2;
+
+        }
+
+        if (num3 > maior)
+
+        {
+
+            maior = num3;
+
+        }
+
+
+        int menor = num1;
+
+
+        if (num2 < menor)
+
+        {
+
+            menor = num2;
+
+        }
+
+        if (num3 < menor)
+
+        {
+
+            menor = num3;
+
+        }
+
+
+        Console.WriteLine("O maior número é: {0}", maior);
+
+        Console.WriteLine("O menor número é: {0}",menor);
+
+    }
+
+}
+
